019a4c8b1b373f6a6eb39615a476b34a679893f3
chinonsonweke <chinonsonweke01@gmail.com>
2022-05-16 03:47:39 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,9 +2,9 @@
 #define _DOG_H_
 /**
  * struct dog -Dog data.
- * name: Name of the dog
- * age: age of the dog.
- * owner: owner of the dog.
+ * @name: Name of the dog
+ * @age: age of the dog.
+ * @owner: owner of the dog.
  * Desctiption: a structure with the name, age and owner of a dog.
 **/
 
